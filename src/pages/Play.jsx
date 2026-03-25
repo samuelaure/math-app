e298cfefd133dae2130e6727278bc89c6942689f
Samuel Aure <samuelaure@gmail.com>
2026-03-25 14:05:29 +0100

    refactor(play): propagate attempt metrics to interactive router bounds

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -35,7 +35,8 @@ export default function Play() {
       status, 
       inputRef, 
       handleKeyDown, 
-      showWhisper
+      showWhisper,
+      attempts
     };
 
     switch (problem.type) {
